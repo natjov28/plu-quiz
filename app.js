417290c0bae31301565547a1e8c15e332cb1cdd3
Viktor <viktor.bajs-jovanovic@ewd.de>
2021-07-31 22:58:41 +0200

    Db init

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+var connected = false;
+//mongodb+srv://test:<password>@cluster0.1ie43.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//DB Connection
+getConnection = async () => {
+    await mongoose.connect(
+      'mongodb+srv://test:1234@cluster0.1ie43.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      }
+    ).then(() => {
+    console.log('Connected to DB!'); connected = true; }).catch(() => console.log('Error DB Connection!'))
+};
+
+getConnection();
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+
+
   });
 
-app.get('/something', (request, res) => {
-    res.send('ide test!!')
+app.get('/connected', (request, res) => {
+    res.send({ DBStatus: connected });
     }
 )
 
